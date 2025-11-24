27586996f8e915f6c130aa87afafb54971569e73
Frederik Bußmann <frederik@bussmann.io>
2025-11-24 23:37:56 +0100

    chore: fix component types export
    Signed-off-by: Frederik Bußmann <frederik@bussmann.io>

--- src/client/index.d.ts
+++ src/client/index.d.ts
@@ -1,10 +1,2 @@
 export interface paths extends Record<string, any> {}
-
-export interface components {
-  schemas: Record<string, any>
-  responses: Record<string, any>
-  parameters: Record<string, any>
-  requestBodies: Record<string, any>
-  headers: Record<string, any>
-  pathItems: Record<string, any>
-}
+export interface components extends Record<string, any> {}
